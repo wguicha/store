06a5a19890953f523e3469a16eb4bc928d5b5f80
WilliamG <williamalberto1@hotmail.com>
2023-01-28 13:03:07 -0500

    Se ha maquetadi la tabla del carrito

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
-import { Cart } from 'src/app/models/cart.model';
+import { Cart, CartItem } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html'
 })
 export class CartComponent {
-  cart: Cart = { items: [] }
+  cart: Cart = { items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'Dodge Journey 2011',
+    price: 70000,
+    quantity: 20,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'Dodge Journey 2009',
+    price: 90000,
+    quantity: 2,
+    id: 2,
+  }]};
+
+  dataSource: Array<CartItem>= [];
+
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ]
+
+  ngOnInit(): void{
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items.map((item) => item.price * item.quantity)
+    .reduce((prev, current) => prev + current, 0)
+  }
 
 }
