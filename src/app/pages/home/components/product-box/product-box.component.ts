41b40c50023ea368ac083caca6da0bec07a63078
WilliamG <williamalberto1@hotmail.com>
2023-01-28 16:05:54 -0500

    Se adiciono el metodo de agregar al carrito

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model'
 
 @Component({
   selector: 'app-product-box',
@@ -6,5 +7,21 @@ import { Component, Input } from '@angular/core';
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+      id: 1,
+      title: 'Sensor Journey 2011-2009',
+      price: 150,
+      category: 'Sensores',
+      description: 'Descripcion del sensor',
+      image: 'https://via.placeholder.com/150',
+  };
+  @Output() addToCart = new EventEmitter();
+
+
+  onAddToCart(): void {
+
+    this.addToCart.emit(this.product);
+
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROWS_HEIGHT: { [id:number]: number } = { 1: 400, 3: 335, 4:350 }
 
@@ -12,6 +14,10 @@ export class HomeComponent {
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
+  constructor(private cartService: CartService){
+
+  }
+
   columnsCountChange(colsNum: number): void{
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[this.cols];
@@ -19,6 +25,17 @@ export class HomeComponent {
 
   onShowCategory(newCategory: string): void{
     this.category = newCategory;
+  }
+
+  onAddToCart(product:Product): void{
+    this.cartService.addToCart({
+      product: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1,
+      id: product.id
+    });
+
   }
 
 }
